4df15c846a9aa96ff5ae6bf8a063f12af9b50a08
HenriqueSerres <henriqueserres@gmail.com>
2022-11-07 16:37:02 -0300

    feat: (app.js , server.js) instanciando servidor e base para rotas

---
+++ backend/src/api/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('../routes');
+const middlewares = require('../middlewares/errorMiddleware');
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use(routes);
+app.use(middlewares.errorMiddleware);
+
+module.exports = app;
