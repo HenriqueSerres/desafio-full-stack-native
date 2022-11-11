6a1677c4af9e30aca9b575fbb9ac3a1e87a2e992
HenriqueSerres <henriqueserres@gmail.com>
2022-11-10 22:57:51 -0300

    add rotas e browserouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import UserProvider from './context/UserProvider';
 import { Route, Switch } from 'react-router-dom'
-import login from './pages/login'
+import Login from './pages/Login'
+import Header from './components/Header';
+import User from './pages/User';
 // import './App.css';
 
 function App() {
   return (
     <UserProvider>
+      <Header />
       <Switch>
-        <Route exact path='/login' component={login} />
+        <Route exact path='/login' component={ Login } />
+        <Route exact path='/user' component={ User } />
       </Switch>
-    </UserProvider>
+    </UserProvider>  
   );
 }
 
